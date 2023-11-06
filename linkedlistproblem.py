25742b3ba230c82ec0160fd2ee578cede4f99808
Rohini Ladhar <rohit.ladhar10@gmail.com>
2023-11-06 16:45:41 +0000

    linked list reverse

--- linkedlistproblem.py
+++ linkedlistproblem.py
@@ -41,14 +41,39 @@ class Linkedlist:
             temp = temp.next
 
         max.data = item
+
+    def sum_odd(self):
+        curr = self.head
+        result = 0
+        index = 0
+        while curr != None:
+            if index % 2 == 1:
+                print(curr.data)
+                result = result + curr.data
+            index = index + 1
+            curr = curr.next
+        return result
         
-        
+    def reverse(self):
+        prev_node = None
+        curr_node = self.head   
+        while curr_node != None:
+            next_node = curr_node.next
+            curr_node.next = prev_node
+            prev_node = curr_node
+            curr_node = next_node
+        self.head  = prev_node
        
 
 L = Linkedlist()
-L.append(1)
-L.append(2)
-L.append(3)
+L.append(13)
+L.append(42)
+L.append(31)
+L.append(14)
+L.append(20)
+L.append(31)
 print(L.transverse())
-L.replace_value(21)
+# L.replace_value(21)
+L.reverse()
+# print(L.sum_odd())
 print(L.transverse())
